a89885c3f9b9f43c633407e6ea5612407d2c4754
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-10-13 00:31:16 +0200

    update snowStorm

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -27,9 +27,9 @@ const Hero = () => {
 
   return (
     <Container>
-      {window !== undefined ? (
+      {typeof window !== undefined && (
         <Snowfall color="white" snowflakeCount={12} />
-      ) : null}
+      )}
 
       <motion.div className="content">
         <div className="title">
