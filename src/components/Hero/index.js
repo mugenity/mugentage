b4fbe40e756210cc3243e859b923f333cb3f1c82
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-10-13 10:10:05 +0200

    hero animation

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Container } from "./styles"
-// import Snowfall from "react-snowfall"
+import Particles from "react-particles-js"
 import { motion } from "framer-motion"
 
 const Hero = () => {
@@ -28,6 +28,59 @@ const Hero = () => {
   return (
     <Container>
       {/* <Snowfall color="white" snowflakeCount={12} /> */}
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 100,
+              density: {
+                enable: false,
+              },
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3,
+              },
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              random: true,
+              speed: 1,
+              direction: "top",
+              out_mode: "out",
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "bubble",
+              },
+              onclick: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 400,
+                duration: 4,
+              },
+            },
+          },
+        }}
+      />
       <motion.div className="content">
         <div className="title">
           <motion.h2 variants={sentence} initial="hidden" animate="visible">
